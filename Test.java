b34c96b227fe1f6f87ffbae8e0d229c56ca86714
党威 <1357711677@qq.com>
2020-08-12 13:38:33 +0800

    Test.java

---
+++ Test.java
@@ -0,0 +1,68 @@
+给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+
+图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+测试用例格式：
+
+简单起见，每个节点的值都和它的索引相同。例如，第一个节点值为 1（val = 1），第二个节点值为 2（val = 2），以此类推。该图在测试用例中使用邻接列表表示。
+
+邻接列表 是用于表示有限图的无序列表的集合。每个列表都描述了图中节点的邻居集。
+
+给定节点将始终是图中的第一个节点（值为 1）。你必须将 给定节点的拷贝 作为对克隆图的引用返回。
+
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/clone-graph
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node,Node> map=new HashMap<>();
+    public Node cloneGraph(Node node) {
+        return dfs(node);
+    }
+    private Node dfs(Node head){
+        if(head==null) return head;
+        if(map.containsKey(head)){
+            return map.get(head);
+        }
+        Node clone=new Node(head.val,new ArrayList<>());
+        map.put(head,clone);
+        for(Node node:head.neighbors){
+            //if(!map.containsKey(node)){
+               clone.neighbors.add(dfs(node));
+               //dfs(node);
+            //}
+        }
+        return clone;
+    }
+}
